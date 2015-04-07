4abc13c2ff117b5452088850a8b590bbccdc5c32
Martyn Davies <martynrdavies@gmail.com>
2015-04-07 13:46:11 +0100

    update tests

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var legit = require('../');
 describe('Testing Emails', function(){
 	describe('Email is legit', function(){
 		it('should return true when email is legit', function(){
-			legit('martyn@sendgrid.com', function(validation, addresses){
+			legit('martyn@sendgrid.com', function(validation, addresses, err){
 				validation.should.be.true;
 				should.exist(addresses);
 			});
@@ -14,9 +14,10 @@ describe('Testing Emails', function(){
 
 	describe('Email is not legit', function(){
 		it('should return false when email is not legit', function(){
-			legit('nosir@neverwouldibuythisdomainladdyohno.com', function(validation, addresses){
+			legit('nosir@neverwouldibuythisdomainladdyohno.com', function(validation, addresses, err){
 				validation.should.be.false;
 				should.not.exist(addresses);
+				should.exist(err);
 			});
 		});
 	});
