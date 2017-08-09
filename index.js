b2c3825b7bf171ad51b14a4d00663708d18e6578
Martyn Davies <martynrdavies@gmail.com>
2017-08-09 17:51:18 +0200

    remove strict

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const dns = require('dns');
 
 module.exports = function(emailAddress, cb) {
-	'use strict';
-	let splitEmail = emailAddress.split('@');
+	var splitEmail = emailAddress.split('@');
 	dns.resolveMx(splitEmail[1], function(err, addresses){
 		if (addresses) {
 			cb(null, true, addresses);
