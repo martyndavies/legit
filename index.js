1ba103b3af756d7d994a85ba5f6b4ed4b46fee49
Ryan Zeigler <zeiglerr@gmail.com>
2017-11-13 14:09:28 -0600

    Differentiate between network errors and no dns or mx record.

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const dns = require('dns');
 
 module.exports = function(emailAddress, cb) {
 	var splitEmail = emailAddress.split('@');
-	dns.resolveMx(splitEmail[1], function(err, addresses){
-		if (addresses) {
-			cb(null, true, addresses);
-		} else {
-			cb(err, false, null);
-		}
+	dns.resolveMx(splitEmail[1], function(err, addresses) {
+        if (err) {
+            if (err.code === 'ENOTFOUND' || err.code === 'ENODATA') {
+                cb(null, false, null);
+            } else {
+                cb(err, false, null);
+            }
+        } else {
+            cb(null, addresses.length > 0, addresses);
+        }
 	});
 };
