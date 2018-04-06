1608a1bab034022c9b46b56317a41ccf4323277c
Martyn Davies <martynrdavies@gmail.com>
2018-04-06 12:07:36 +0100

    Change from tabs to spaces

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const dns = require('dns');
 
 module.exports = function(emailAddress, cb) {
-	var splitEmail = emailAddress.split('@');
-	dns.resolveMx(splitEmail[1], function(err, addresses){
-		if (addresses) {
+  var splitEmail = emailAddress.split('@');
+    dns.resolveMx(splitEmail[1], function(err, addresses){
+      if (addresses) {
 			cb(null, true, addresses);
 		} else {
 			cb(err, false, null);
