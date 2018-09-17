81617a43b90ec71217b14f84899d3572ec7c9878
Martyn Davies <martynrdavies@gmail.com>
2018-09-17 16:41:01 +0100

    update error and bump to 1.0.5

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-const dns = require("dns");
+const dns = require('dns');
 
 const validateEmailAddress = emailAddress => {
   return new Promise((resolve, reject) => {
-    const splitEmail = emailAddress.split("@");
+    const splitEmail = emailAddress.split('@');
 
     dns.resolveMx(splitEmail[1], (err, mx) => {
-      if (typeof mx != "undefined") {
+      if (typeof mx != 'undefined') {
         mx
           ? resolve({ isValid: true, mxArray: mx })
           : resolve({ isValid: false, mxArray: null });
       } else {
-        reject(err);
+        reject(new Error(err));
       }
     });
   });
