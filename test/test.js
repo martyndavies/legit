30c6d4e4ae0796ab3434e01dc2e46135fba01ec4
Martyn Davies <martynrdavies@gmail.com>
2019-04-01 17:59:55 +0200

    update test

--- test/test.js
+++ test/test.js
@@ -1,19 +1,22 @@
-const assert = require("assert");
-const should = require("should");
-const legit = require("../src");
+const assert = require('assert');
+const should = require('should');
+const legit = require('../src');
 
-describe("Testing Emails", function() {
-  describe("Email is legit", function() {
-    it("should return true when email is legit", function() {
-      legit("martyn@martyndavies.me").then(
-        result => result.isValid.should.be.true && should.exist(result.mxArray)
-      );
+describe('Testing Emails', function() {
+  describe('Email is legit', function() {
+    it('should return true when email is legit', function() {
+      legit('martyn@nexmo.com')
+        .then(
+          result =>
+            result.isValid.should.be.true && should.exist(result.mxArray)
+        )
+        .catch(err => should.exist(err.errors));
     });
   });
 
-  describe("Email is not legit", function() {
-    it("should return false when email is not legit", function() {
-      legit("nosir@neverwouldibuythisdomainladdyohno.com")
+  describe('Email is not legit', function() {
+    it('should return false when email is not legit', function() {
+      legit('nosir@neverwouldibuythisdomainladdyohno.com')
         .then(result => result.isValid.should.not.exist)
         .catch(err => should.exist(err.errors));
     });
