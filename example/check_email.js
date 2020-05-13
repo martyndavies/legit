ab97816dcc868c7d5482697a7158f39ece46fbe7
Martyn Davies <martynrdavies@gmail.com>
2020-05-13 14:10:46 +0200

    change test to broken domain

--- example/check_email.js
+++ example/check_email.js
@@ -1,6 +1,6 @@
 const legit = require("../src/index.js");
 
-legit("martyn@martyndavies.me")
+legit("martyn@notadomainzzzzzzzzz.me")
   .then(result => {
     result.isValid ? console.log("Valid!") : console.log("Invalid!");
     console.log(JSON.stringify(result));
